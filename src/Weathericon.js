58e79baf8fc2904ceba0fe33d30164872e6f703b
Greta <greta@saffer.de>
2019-06-04 23:19:48 +0100

    trying to integrate animated weathericons

--- src/Weathericon.js
+++ src/Weathericon.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactAnimatedWeather from "react-animated-weather";
+import PropTypes from "prop-types";
 
-const defaults = {
-  icon: "CLEAR_DAY",
-  color: "goldenrod",
-  size: 512,
-  animate: true
-};
+export default class WeatherIcon extends Component {
+  static propTypes = { iconName: PropTypes.string.isRequired };
 
-const App = () => (
-  <ReactAnimatedWeather
-    icon={defaults.icon}
-    color={defaults.color}
-    size={defaults.size}
-    animate={defaults.animate}
-  />
-);
+  iconMatching = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "CLOUDY",
+    "03n": "CLOUDY",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "RAIN",
+    "09n": "RAIN",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "SLEET",
+    "11n": "SLEET",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG"
+  };
 
-export default App;
+  render() {
+    return (
+      <ReactAnimatedWeather
+        icon={this.iconMatching[this.props.iconName]}
+        color="#000"
+        size={38}
+        animate={true}
+      />
+    );
+  }
+}
